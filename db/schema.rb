416140ea717d6313ce87e36da2fdd5661300648c
tracy sikenjana <unboxedcanvas@gmail.com>
2022-11-03 12:54:22 +0200

    Added User location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_03_094214) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_03_105318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,6 +78,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_03_094214) do
     t.text "bio"
     t.string "batch_number"
     t.string "github"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
