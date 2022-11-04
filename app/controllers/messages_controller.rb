ad6ff34597c67db206e52665dfbceb6b673c67f1
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2022-11-04 11:53:36 +0200

    chat feature working

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,18 +6,19 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
-    @conversation   = Conversation.find(params[:conversation_id])
+    @conversation = Conversation.find(params[:conversation_id])
     @message.conversation = @conversation
     @message.user = current_user
 
-    @message.save
-    ConversationChannel.broadcast_to(
-      @conversation,
-      render_to_string(
-        partial: "messages/messages", locals: {message: @message}
+    if @message.save
+      ConversationChannel.broadcast_to(
+        @conversation,
+        render_to_string(
+          partial: "messages/messages", locals: {message: @message}
+        )
       )
-    )
-    head :ok
+      head :ok
+    end
   end
 
   private
--- app/javascript/controllers/conversation_subscription_controller.js
+++ app/javascript/controllers/conversation_subscription_controller.js
@@ -1,27 +1,29 @@
-import { Controller } from "@hotwired/stimulus"
-import { createConsumer } from "@rails/actioncable"
+import { Controller } from "@hotwired/stimulus";
+import { createConsumer } from "@rails/actioncable";
 
 // Connects to data-controller="conversation-subscription"
 export default class extends Controller {
   static values = {
-    ConversationId: Number }
-
-    static targets = ["messages"]
+    conversationId: Number,
+  };
+  static targets = ["messages"];
 
   connect() {
-    console.log(this.ConversationIdValue
-    )
     this.channel = createConsumer().subscriptions.create(
-      { channel: "ConversationChannel", id: this.ConversationIdValue },
-      { received: (date) => { this.#insertMessageAndScrollDown(data) } }
-    )
+      { channel: "ConversationChannel", id: this.conversationIdValue },
+      {
+        received: (data) => {
+          this.#insertMessageAndScrollDown(data);
+        },
+      }
+    );
   }
   resetForm(event) {
-    event.target.reset()
+    event.target.reset();
   }
 
   #insertMessageAndScrollDown(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
-    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+    this.messagesTarget.insertAdjacentHTML("beforeend", data);
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight);
   }
 }
